da750674e6f7956f407c599f644fe44ff4ef4b80
Andrey Lilitkin <vol.igor98@yandex.ru>
2017-12-12 14:11:33 +1000

    Commit missing files

---
+++ src/components/Bottom.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Buttom extends React.Component {
+	render() {
+		return (
+			<div className="page-border bottom colors-e background-solid">
+				<a rel="noopener noreferrer" target="_blank" href="https://blog.xo1.io/"><i className="fa fa-medium fa-2x"></i></a>
+				<a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/SecureVote/"><i className="fa fa-facebook fa-2x"></i></a>
+				<a rel="noopener noreferrer" target="_blank" href="https://twitter.com/Secure_Vote"><i className="fa fa-twitter fa-2x"></i></a>
+				<a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/company/18001068/"><i className="fa fa-linkedin fa-2x"></i></a>
+			</div>
+		)
+	}
+}
